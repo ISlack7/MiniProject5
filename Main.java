bd168fa87c5a1b4c34bf08ba84b41f37b19d7cbb
ISlack7 <ian.slack@patelhs.org>
2021-02-22 17:08:01 +0000

    I got the code to not be an infinate loop and print out the correct number.

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    
+		Scanner scan = new Scanner(System.in);
+		//trying to get the amount of students in a class to gauge how many students need to be added
+    System.out.println("how many students are in your class?");
+			int numStudents = scan.nextInt();
+			//trying to get the maximum number of students to figure out how many students can be in one class
+		System.out.println("what is the maximum number of students allowed?");
+			int maxStudents = scan.nextInt();
+			//setting the variable classSizes equal to the maximum amount of students in order to call my method
+			int classSizes = classSize(maxStudents);
+			System.out.println("Your class size is " + classSizes);
   }
-
+		public static int classSize(int students){
+			int class1Size = students;
+			if (students == 10)
+					return 10;		
+			else 
+			//If the number is less than the maximum number of students, it adds 1 to the number until it equals the maximum amount of students.
+					return class1Size + classSize(students + 1);
+				 
+		}
 }
