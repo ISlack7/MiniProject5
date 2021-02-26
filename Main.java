36eef81e729b2d23088245e03f616fc2dfc09cbd
ISlack7 <ian.slack@patelhs.org>
2021-02-26 13:39:50 +0000

    Added an else if to account for a class size over 10 students.

--- Main.java
+++ Main.java
@@ -3,22 +3,29 @@ class Main {
   public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		//trying to get the amount of students in a class to gauge how many students need to be added
-    System.out.println("how many students are in your class?");
+    System.out.println("How many students are in your class?");
 			int numStudents = scan.nextInt();
-			//trying to get the maximum number of students to figure out how many students can be in one class
-		System.out.println("what is the maximum number of students allowed?");
-			int maxStudents = scan.nextInt();
+			//setting the maximum amount of students equal to ten
+			int maxStudents = 10;
 			//setting the variable classSizes equal to the maximum amount of students in order to call my method
 			int classSizes = classSize(maxStudents);
 			System.out.println("Your class size is " + classSizes);
   }
 		public static int classSize(int students){
 			int class1Size = students;
-			if (students == 10)
-					return 10;		
-			else 
-			//If the number is less than the maximum number of students, it adds 1 to the number until it equals the maximum amount of students.
+			if (students == 10){
+				return 10;
+			}
+			else if(students > 10){
+				return class1Size - classSize(students - 1);
+			}
+					
+			else {
+			//If the number is less than the maximum number of students, it adds 1 to the number until it equals the maximum amount of students. It knows that the maximum amount of students is ten so if the number is less than ten, it runs this until the class size/students is equal to ten.
 					return class1Size + classSize(students + 1);
+			}
+
+					
 				 
 		}
 }
